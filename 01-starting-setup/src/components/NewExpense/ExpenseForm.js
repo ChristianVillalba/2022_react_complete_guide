8542b99743d973e607ab1388ee861c4fb28ee2f0
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-03-16 21:56:33 +0000

    adds state to all the inputs in ExpenseForm.js

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,28 +1,65 @@
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-function ExpenseForm() {
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  function titleChangeHandler(event) {
+    setEnteredTitle(event.target.value);
+  }
+
+  function amountChangeHandler(event) {
+    setEnteredAmount(event.target.value);
+  }
+
+  function dateChangeHandler(event) {
+    setEnteredDate(event.target.value);
+  }
+
+  //   function titleChangeHandler(event) {
+  //     const { title, value } = event.target;
+  //     setTitle((prevTitle) => {
+  //       return {
+  //         ...prevNote,
+  //         [title]: value,
+  //       };
+  //     });
+  //   }
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
-            <label>Title</label>
-            <input type="text" />
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-            <label>Amount</label>
-            <input type="number" min="0.01" step="0.01"/>
+          <label>Amount</label>
+          <input
+            type="number"
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01"
+          />
         </div>
         <div className="new-expense__control">
-            <label>Date</label>
-            {/* min and max params will be useful when we add a filter */}
-            <input type="date" min="2022-01-01" max="2025-12-31"/>
+          <label>Date</label>
+          {/* min and max params will be useful when we add a filter */}
+          <input
+            type="date"
+            onChange={dateChangeHandler}
+            min="2022-01-01"
+            max="2025-12-31"
+          />
         </div>
         <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
+          <button type="submit">Add Expense</button>
         </div>
       </div>
     </form>
   );
-}
+};
 
 export default ExpenseForm;
