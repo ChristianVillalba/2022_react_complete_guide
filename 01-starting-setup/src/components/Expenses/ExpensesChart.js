884970f9718b3c48dcccacad0909ee463cdb7af0
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-04-03 22:25:55 +0100

    adds chartDataProps objects and a way to update them in order to passthem to <Chart/>

--- 01-starting-setup/src/components/Expenses/ExpensesChart.js
+++ 01-starting-setup/src/components/Expenses/ExpensesChart.js
@@ -2,8 +2,29 @@ import React from "react";
 import Chart from "../Chart/Chart";
 
 function ExpensesChart(props) {
+    const chartDataPoints = [
+        {label: "Jan", value: 0},
+        {label: "Feb", value: 0},
+        {label: "Mar", value: 0},
+        {label: "Apr", value: 0},
+        {label: "May", value: 0},
+        {label: "Jun", value: 0},
+        {label: "Jul", value: 0},
+        {label: "Aug", value: 0},
+        {label: "Sep", value: 0},
+        {label: "Oct", value: 0},
+        {label: "Nov", value: 0},
+        {label: "Dec", value: 0}
+    ];
+    for (const expense in props.expenses) {
+        const expenseMonth = expense.data.getMonth(); // Starting at 0 == Jan
+        // expenseMonth will be used as index (0, 11)
+        chartDataPoints[expenseMonth].value += expense.amount;
+        // the values that start at 0 will be updated and passed to <Chart />
+    }
   return (
-      <Chart />
+      // dataPoints is the props we recieve from Chart.js and we set it equal to our update (above)
+      <Chart dataPoints/>
   );
 }
 
