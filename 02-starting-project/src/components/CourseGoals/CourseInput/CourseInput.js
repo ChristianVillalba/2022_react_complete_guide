e248c8ddd9c784ff8d5c1fcbb9940d74d2f586a6
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-04-04 23:51:57 +0100

    adds functionality to CourseInput.js: check if the input is valid and set dynamic style depending on it

--- 02-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 02-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [inputIsValid, setinputIsValid] = useState(true);
+  // by default, we trust that the input is valid
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    // Check if the input is empty
+    if (enteredValue.trim().length === 0) {
+      setinputIsValid(false); // the input is NOT valid
+      return; // if the result is 0, the next line of code won't be executed
+    }
     props.onAddGoal(enteredValue);
   };
 
