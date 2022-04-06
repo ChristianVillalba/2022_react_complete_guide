c019fa8174c9a5860b7324791335d25df46efe58
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-04-06 21:38:28 +0100

    adds dynamically changing parts of the styles

--- 02-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 02-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -7,35 +7,30 @@ import styled from "styled-components";
 const FormControl = styled.div`
   margin: 0.5rem 0;
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-}
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props =>( props.invalidProp ? "red" : "black")} ;
+  }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props =>( props.invalidProp ? "red" : "#ccc")};
+    background: ${props =>( props.invalidProp ? "salmon" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
 
-&.invalid-input input {
-  border-color: red;
-  background: salmon;
-}
-
-&.invalid-input label{
-  color: red;
-}`;
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -61,10 +56,12 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isInputValid && "invalid-input"}>
+      {/* <FormControl className={!isInputValid && "invalid-input"}> */}
+      {/* Feature provided by styled-components: adding props to our Styled Component */}
+      <FormControl invalidProp={!isInputValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-        </FormControl>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
