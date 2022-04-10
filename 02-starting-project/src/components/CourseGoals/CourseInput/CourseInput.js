3bceb3591a48e68b3fcfe4e9788f5fa6c6796729
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-04-10 23:18:02 +0100

    adds CourseInput styled using dynamic styles with CSS modules. ready to add media queries

--- 02-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 02-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,36 +1,40 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
-import styled from "styled-components";
+import styles from "./CourseInput.module.css";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// Using Styled-Components
+// import styled from "styled-components";
+// import { Component } from "react/cjs/react.production.min";
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props =>( props.invalidProp ? "red" : "black")} ;
-  }
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props =>( props.invalidProp ? "red" : "#ccc")};
-    background: ${props =>( props.invalidProp ? "salmon" : "transparent")};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props =>( props.invalidProp ? "red" : "black")} ;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-  
-`;
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props =>( props.invalidProp ? "red" : "#ccc")};
+//     background: ${props =>( props.invalidProp ? "salmon" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
+
+
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -58,10 +62,14 @@ const CourseInput = (props) => {
     <form onSubmit={formSubmitHandler}>
       {/* <FormControl className={!isInputValid && "invalid-input"}> */}
       {/* Feature provided by styled-components: adding props to our Styled Component */}
-      <FormControl invalidProp={!isInputValid}>
+      <div className={`
+      ${styles[`form-control`]} 
+      // invalid-input name changed to invalid
+      ${!isInputValid && styles.invalid}
+       `}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
