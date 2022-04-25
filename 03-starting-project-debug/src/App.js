2d85a036ef20f4547b83769fd35e91e5f6adcbc0
Christian Villalba UK <christianvillalba.uk@gmail.com>
2022-04-25 08:20:51 +0100

    generates id's / keys using Math() func in App.js

--- 03-starting-project-debug/src/App.js
+++ 03-starting-project-debug/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   const addGoalHandler = (enteredText,goalId) => {
     setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: goalId });
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
     });
   };
